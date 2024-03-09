1a32c7009508b8e6c742b96fbd6a63d3f99b9e31
liel gerbi <lielgerbi2000@gmail.com>
2024-03-09 18:42:33 +0200

    change to https

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const https = require('http');
+const fs = require('fs');
 const mongoose = require('mongoose');
 const socketIo = require('socket.io');
 const users = require('./routes/user');
