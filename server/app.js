e0eb3fac170508ddc5cb66de0c30d8e8e24dc3fb
liel gerbi <lielgerbi2000@gmail.com>
2024-03-10 20:27:21 +0200

    add logs

--- server/app.js
+++ server/app.js
@@ -49,10 +49,10 @@ const corsOptions = {
 
 const setupApp = new Promise((resolve, reject) => {
   var db = mongoose.connection;
-  mongoose.connect(`mongodb+srv://lielgerbi2000:135792468i@cluster0.b7saegs.mongodb.net`, {
-  })
-     // mongoose.connect(`mongodb://admin:bartar20%40CS@10.10.248.226:21771/`, {
-//})
+  // mongoose.connect(`mongodb+srv://lielgerbi2000:135792468i@cluster0.b7saegs.mongodb.net`, {
+  // })
+     mongoose.connect(`mongodb://admin:bartar20%40CS@10.10.248.226:21771/`, {
+})
 
   .then(() => {
     db = db.useDb('give&take')
